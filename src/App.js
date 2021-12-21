70aacffdcec181f163fa6c9f0bf3270ebfd45377
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-12-21 23:53:49 +0000

    Add components to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import React from "react";
+import "./styling/App.scss";
+import { Header } from "./components/Header";
+import { ProjectCard } from "./components/ProjectCard";
+import { AboutMe } from "./components/AboutMe";
+import { Contact } from "./components/Contact";
+import { Welcome } from "./components/Welcome";
+import { motion } from "framer-motion";
 
-function App() {
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 2,
+    },
+  },
+};
+export const App = () => {
   return (
-    <div className="App">
-    Hello word
-    </div>
+    <motion.div
+    variants={containerVariants}
+    initial="hidden"
+    animate="visible">
+      <Header />
+      <Welcome />
+      <ProjectCard />
+      <AboutMe />
+      <Contact />
+    </motion.div>
   );
-}
-
-export default App;
+};
